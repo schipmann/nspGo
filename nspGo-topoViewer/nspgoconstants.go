61f8bac6fec166542de8dc6e9b4788fba25ccd86
schipmann <henryschipmann@gmail.com>
2022-03-02 14:17:40 +0100

    1. merge and sort files

---
+++ nspGo-topoViewer/nspgoconstants.go
@@ -0,0 +1,10 @@
+package nsptopoviewer
+
+// valid for NSP 21.6
+//
+const GLBL_NSP_SESSION_URL_TOKEN = "/rest-gateway/rest/api/v1/auth/token"
+const GLBL_NSP_SESSION_URL_TOKEN_REVOKE = "/rest-gateway/rest/api/v1/auth/revocation"
+const GLBL_NSP_IP_OPTIM_BASE_URL = ":8543/sdn/api/v4/"
+const GLBL_NSP_RESTCONF_BASE_URL = ":8545/restconf/"
+const GLBL_NSP_IETF_NETWORKS = ":8543/sdn/api/v4/ietf/te/networks"
+const GLBL_NSP_SERVICE_SESSION = ":8545/restconf/data/nsp-service-intent:intent-base/intent=EVPN-EPIPE-InterAS-optionB,evpn-epipe"
